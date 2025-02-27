b0280d4b88d55f8ffa24d7ed160f3a101bdcea41
Arcdarelle <162192195+Arcdarelle@users.noreply.github.com>
2025-02-27 12:01:01 +0100

    Update main.tf

--- terraform_variableand_provisioners/main.tf
+++ terraform_variableand_provisioners/main.tf
@@ -12,6 +12,67 @@ terraform {
   }
 }
 
+# Create a VPC
+
+resource "aws_vpc" "lab_vpc" {
+
+  cidr_block           = var.VPC_cidr
+  enable_dns_support   = "true" #gives you an internal domain name
+  enable_dns_hostnames = "true" #gives you an internal host name
+  instance_tenancy     = "default"
+
+  tags = {
+    Name = "${var.project-name}-VPC"
+  }
+
+}
+
+# Create an Internet Gateway
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.lab_vpc.id
+
+  tags = {
+    Name = "${var.project-name}-igw"
+  }
+}
+
+# Create a route table
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.lab_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project-name}-public-route-table"
+  }
+}
+
+# Associate the route table with the public subnet
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Create a public subnet
+
+resource "aws_subnet" "public_subnet" {
+
+  vpc_id                  = aws_vpc.lab_vpc.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = var.AZ
+
+  tags = {
+    Name = "${var.project-name}-public-subnet"
+  }
+}
+
 # use data source to get a registered amazon linux 2 ami
 data "aws_ssm_parameter" "ami_id" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
@@ -21,7 +82,7 @@ data "aws_ssm_parameter" "ami_id" {
 resource "aws_instance" "ec2_instance" {
   ami                    = data.aws_ssm_parameter.ami_id.value
   instance_type          = var.typeinstances
-  subnet_id              = aws_default_subnet.default_az1.id
+  subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.webserver_security_group.id]
   key_name               = aws_key_pair.webserver_key.key_name
 
@@ -45,4 +106,3 @@ resource "aws_instance" "ec2_instance" {
     Name = "Webserver httpd"
   }
 }
-
