a26c54ab998f8f8211317bd9b50862f894f57f34
Arcdarelle <162192195+Arcdarelle@users.noreply.github.com>
2025-02-27 12:03:10 +0100

    Update variables.tf

--- terraform_variableand_provisioners/variables.tf
+++ terraform_variableand_provisioners/variables.tf
@@ -1,5 +1,6 @@
+
 variable "region" {
-  type    = string
+  type = string
   default = "us-east-1"
 }
 
@@ -7,4 +8,25 @@ variable "typeinstances" {
   type        = string
   default     = "t2.micro"
   description = "type of the instances"
+}
+
+
+variable "VPC_cidr" {
+  type = string
+  default = "192.168.0.0/16" 
+}
+
+variable "public_subnet_cidr" {
+  type = string
+  default = "192.168.1.0/24"
+}  
+
+variable "AZ" {
+  type = string
+  default = "us-east-1a"
+}
+
+variable "project-name" {
+  type    = string
+  default = "Terraform-variables-lab"
 }
